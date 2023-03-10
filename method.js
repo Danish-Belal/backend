5cf7b34f59e81a96ea545118819fb4ac8f34756f
Danish-Belal <danishexplore019@gmail.com>
2023-03-10 18:03:14 +0530

    🔥Parameters and Queries

--- method.js
+++ method.js
@@ -2,10 +2,36 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-let user = {};
+let user = [
+     {
+          id :1,
+          Name:"Danish",
+          age:22
 
+     },
+     {
+          id :2,
+          Name:"Belal",
+          age:20
+
+     },
+     {
+          id :3,
+          Name:"Zishan",
+          age:23
+
+     },
+];
+
+//with query
 app.get('/user' , (req , res) => {
-     res.send(user);
+    
+     console.log(req.query);
+     let {Name , age} = req.query;
+     let filterdData = user.filter(userObj =>{
+          return (userObj.Name == Name && userObj.age==age)
+     })
+     res.send(filterdData);
 });
 
 app.post('/user' , (req , res) =>{
@@ -35,5 +61,17 @@ app.delete('/user' , (req , res) =>{
           massageg :" Data is Deleted"
      })
 })
+
+
+// Parameters.
+
+app.get('/user/:name' , (req , res) =>{
+     console.log(req.params.name);
+     // let {id} = req.params;
+     // let user = db.findOne(id);
+     res.json({
+          msg:"user id is", "obj":req.params 
+     })
+});
 
 app.listen(5000);
