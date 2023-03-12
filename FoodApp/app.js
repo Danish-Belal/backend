131acf881e6500f4ca7d6cb3f5b3b30bf5ae2cf0
Danish-Belal <danishexplore019@gmail.com>
2023-03-12 23:26:27 +0530

    🔥Connected postSignup with Mondodb

--- FoodApp/app.js
+++ FoodApp/app.js
@@ -37,7 +37,7 @@ app.use('/auth' , authRouter);
 
 userRouter
      .route("/")
-     .get(middleware1 , getUser , middleware2)
+     .get(getUser)
      .post(postUser)
      .patch(updateUser)
      .delete(deleteUser)
@@ -52,13 +52,13 @@ function middleware1(req , res , next){
      next();
 }
 
-function middleware2(req , res){
-     console.log("Middleware2 is called");
+// function middleware2(req , res){
+//      console.log("Middleware2 is called");
 
-     res.json({msg:"user returned"});
-}
+//      res.json({msg:"user returned"});
+// }
 
-function getUser(req , res , next){   
+async function getUser(req , res ){   
      console.log(req.query);
      let {Name , age} = req.query;
      // let filterdData = user.filter(userObj =>{
@@ -68,8 +68,12 @@ function getUser(req , res , next){
 
      // res.send(user);  
      console.log("getUser called");
-     next();
+     // next();
 
+     let allUser = await userModel.findOne({name:"Dani"});
+     res.json({
+          msg : "user Retrive" , allUser
+     });
 }
 
 function postUser(req  ,res){
@@ -116,15 +120,24 @@ function getSignup(req,res){
      res.sendFile("/public/index.html" , {root: __dirname});
 }
 
-function postSignup(req , res){
-     let {email , name , password} = req.body;
-     console.log(req.body);
-     res.json({
-          msg:"user Signed up",
-          email,
-          name , 
-          password
-     })
+async function postSignup(req , res){
+     // let {email , name , password} = req.body;
+     try{
+          let data = req.body;
+          let user = await userModel.create(data);
+          console.log(data);
+
+          res.json({
+               msg:"user Signed Up",
+               user
+          })
+     }
+
+     catch(err){
+          res.json({
+               err:err.massage
+          })
+     }
 }
 app.listen(5000);
 
@@ -136,3 +149,44 @@ mongooes.connect(data_link)
      .catch(function (err){
           console.log(err);
      })
+
+     // Create schema.
+     const userSchema = ({
+          name:{
+               type : String,
+               required : true,
+          },
+          email:{
+               type:String,
+               required:true,
+               unique : true
+          },
+          password:{
+               type:String,
+               required:true,
+               unique : true,
+               minLength:7,
+          },
+          confirmPassword:{
+               type:String,
+               required:true,
+               unoque : true,
+               minLength:7,
+          },
+         
+     });
+
+     // Models
+     const userModel = mongooes.model("userModel" , userSchema);
+
+     // for checking purpose
+     // (async function createuser(req , res){
+     //      let user = {
+     //           name : "Belal",
+     //           email: "danishbelal@gmail.com",
+     //           password:"1234445678",
+     //           confirmPassword:"1234445678"
+     //      }
+     //      let data = await userModel.create(user);
+     //      console.log(data);
+     // })();
