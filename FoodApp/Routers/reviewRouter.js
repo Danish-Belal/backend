ffe4154caa4d044292dde32e0ff2d798c2455cbd
Danish-Belal <danishexplore019@gmail.com>
2023-04-10 23:55:15 +0530

    Review Router

--- FoodApp/Routers/reviewRouter.js
+++ FoodApp/Routers/reviewRouter.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const reviewRouter = express.Router();
-const {isAuthorized , protectRoute} = require('../helper')
+const {isAuthorised , protectRoute} = require('../helper');
+const {getAllReviews , top3Review ,getPlanReview
+      , createReview , updateReview , deleteReview} = require('../controller/reviewController')
 
 reviewRouter
      .route('/all')
@@ -9,6 +11,7 @@ reviewRouter
      .route("/top3")
      .get(top3Review)
 
+reviewRouter.use(protectRoute);
 reviewRouter
      .route('/:id')
      .get(getPlanReview);
@@ -17,6 +20,7 @@ reviewRouter
      .route(" ")
      .post(createReview);
 
+reviewRouter.use(isAuthorised(['admin' , 'restaurentowner']))
 reviewRouter
      .route("")
      .patch(updateReview)
