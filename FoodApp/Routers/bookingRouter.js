61e08d01aa5038f85e15e5e25e216ba074e1f56b
Danish-Belal <danishexplore019@gmail.com>
2023-04-24 02:46:30 +0530

    🐛 Fixed Issues

--- FoodApp/Routers/bookingRouter.js
+++ FoodApp/Routers/bookingRouter.js
@@ -2,11 +2,18 @@ const express = require('express');
 const bookingRouter = express.Router();
 const {protectRoute} = require('../helper');
 const {createSession} = require('../controller/bookingController');
+const path = require('path');
+const absolutePath = path.join(__dirname, 'public', 'booking.html');
 
+
+bookingRouter.use(express.static("public"));
 bookingRouter.route('/createSession').get(function (req, res) {
-     res.sendFile("C:\Users\Danish Belal\Desktop\Danish Belal\Backend\backend\FoodApp\public\booking.html");
+  
+     res.sendFile(path.join(__dirname, '..', 'public', 'booking.html'));
+
+    
  });
- bookingRouter.use(protectRoute);
+//  bookingRouter.use(protectRoute);
  bookingRouter.route('/createSession').post(createSession);
  
  module.exports = bookingRouter;
--- FoodApp/controller/bookingController.js
+++ FoodApp/controller/bookingController.js
@@ -1,46 +1,48 @@
 let SK = "sk_test_51MvSNOSFWRdncai6TrbUlwL04iCd4oMrTyhq0NT2GjrQSfgOUgn45A7r8lEL6Bh2FPMHX9CswDW0fNbSStE58Qi800gQyTV9bW";
+// let SK = "sk_test_pL1X84CZrOSOYRkyyvKuCwR000a36t5jwK"
 const stripe = require('stripe')(SK);
 const planModel = require('../models/planModel');
 const userModel = require('../models/userModel');
 
-module.exports.createSession = async function(req,res){
-     try{
-          // let userId = req.id;
-          // let planId = req.params.id;
-
-          // const user = await userModel.findById(userId);
-          // const plan = await planModel.findById(planId);
-
-          const session = await stripe.checkout.session.create({
-               // payment_method_type : ['card'],
-               // customer_email : user.email,
-               // client_reference_id : plan.id,
-               line_items: [
-                    {
-                      // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                          name:"HealthyFood101" ,
-                         //  description: plan.description,
-                         //  ammount: plan.price * 100,
-                         amount:"1234",
-                         currency: "inr",
-                         quantity: 1,
-                    
-                    },
-                  ],
-                  mode: "payment",
-                  success_url: `${req.protocol}://${req.get("host")}/profile`,
-                  cancel_url: `${req.protocol}://${req.get("host")}/profile`,
-
-          });
-          // res.json({
-          //      msg: "success",
-          //      session
-          //  });
-          res.redirect(303, session.url);
+module.exports.createSession = async function (req, res) {
+     try {
+         // let userId = req.id;
+         // let planId = req.params.id;
+ 
+         // const user = await userModel.findById(userId);
+         // const plan = await planModel.findById(planId);
+ 
+         const session = await stripe.checkout.sessions.create({
+          // Need to mention few keys explicitly  
+          payment_method_types: ["card"],
+         //   customer_email: user.email,
+         //   client_reference_id: plan.id,
+         line_items: [
+          {
+              quantity: 1,
+              price_data: {
+                  currency: "inr",
+                  unit_amount: 20 * 100,
+                  product_data: {
+                      description: "My Testing PLan Desc.",
+                      name: "My Testing PLan"
+                  },
+              }
+          },
+      ],
+           mode: "payment",
+           success_url: `${req.protocol}://${req.get("host")}/profile`,
+           cancel_url: `${req.protocol}://${req.get("host")}/profile`,
+         });
+         res.json({
+             msg: "success",
+             session
+         });
+         res.redirect(303, session.url);
      }
-     catch(err){
-          res.json({
-               msg: err.msg
-          })
+     catch (err) {
+         res.json({
+             err:err.message
+         })
      }
-}
+ }
