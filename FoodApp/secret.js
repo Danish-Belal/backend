747c0a69ded14bf4234c59a7bf06555fc6d06301
Danish-Belal <danishexplore019@gmail.com>
2023-06-25 18:13:58 +0530

    Reconstruct folders

--- FoodApp/secret.js
+++
@@ -1,3 +0,0 @@
-const data_link = 'mongodb+srv://foodApp101:FoodAppTest@cluster0.2xofzjb.mongodb.net/?retryWrites=true&w=majority'
-const JWT_KEY = "zdsfxcg234w5e6cg";
-module.exports= {data_link , JWT_KEY}
