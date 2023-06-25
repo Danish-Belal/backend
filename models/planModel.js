16349e1d6ebc54082ba6d3cd3da3547883547459
Danish-Belal <danishexplore019@gmail.com>
2023-06-25 18:21:31 +0530

    :abacus: Models

---
+++ models/planModel.js
@@ -0,0 +1,58 @@
+const mongooes = require('mongoose');
+const {data_link} = require('../secret');
+
+
+mongooes
+  .connect(data_link)
+  .then(function (db) {
+    console.log("Plan DB connected");
+    // console.log(db);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+  const planSchem = mongooes.Schema({
+    name : {
+      type:String,
+      require:true,
+      unique : true,
+      maxLength: [20, `Plan name should not exceed 20 characters`]
+    },
+    duration :{
+      type:Number,
+      required : true
+    },
+    price:{
+      type:Number,
+      required:[true , 'price not entered']
+    },
+    discount:{
+      type:Number,
+      validate:[function(){
+        return this.discount < 100
+      }, `discount cannot be 100%`]
+    },
+    ratingsAverages :{
+      type:Number
+    }
+  });
+
+  const planModel = mongooes.model("planModel" , planSchem);
+  module.exports = planModel;
+
+  // (async function createPlan(){
+  //   let plan = {
+  //     name: "Strict fast food",
+  //     duration :300,
+  //     price:90000,
+  //     ratingsAverages : 4.9,
+  //     discount:15
+  //   }
+
+  
+  //   let data = await planModel.create(plan);
+  //   console.log(data);
+  //   // const doc = new planModel(plan);
+  //   // await doc.save();
+  // })();
