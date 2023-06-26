253952137fe3467aaaaa9e427d8029e11d38f345
Danish-Belal <danishexplore019@gmail.com>
2023-06-26 11:33:34 +0530

    :adhesive_bandage: CORS implemented

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express')
-const app = express();
-
 const cookieParser = require("cookie-parser")
+const app = express();
+var cors = require('cors')
+app.use(cors())
+app.use(express.static('public/build'));
+app.use(express.json());
+app.use(cookieParser());
+
+// app.options('*', cors()) // To allow preflight
+
 const userRouter = require('./Routers/userRouter')
 const planRouter = require('./Routers/planRouter');
 const reviewRouter = require('./Routers/reviewRouter');
 const bookingRouter = require('./Routers/bookingRouter');
 
-app.use(express.json());
-app.use(cookieParser());
+
 
 
 
@@ -18,5 +24,6 @@ app.use("/review" , reviewRouter);
 app.use('/booking', bookingRouter);
 
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000
+app.listen(PORT);
 
